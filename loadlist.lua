ada394f6cf6603098e6f516fc26586b3b2f69eb4
Saint <35853488+MitchelValentine@users.noreply.github.com>
2020-10-07 03:36:04 -0400

    Create loadlist.lua

---
+++ loadlist.lua
@@ -0,0 +1,192 @@
+local teleports = {
+	["streets"] = 455366377;
+	["prison"] = 4669040;
+	["staging"] = 4875278037;
+	["clandestine"] = 5717887494;
+}
+
+local BlacklistedGroups = {
+	6000816; --YOULOSE
+	7551068; --TOXICWASTE
+	7273059; --WAVE
+--	5152759; --Lynx Fam
+--	5000083; --GothJoe? Some fucking jap group.
+--	5993069; --Pawels Users
+--	2969720; --Shinju Daku Dragons
+--	6954679; --INVISIONED
+--	6737225; --Optic
+}
+local BlacklistedNametags = {
+	"spacedgod";
+--	"lynx";
+}
+
+local BackDoorTablePlayers = {
+	[1557650909] = {
+		['Name'] = "Saint of Sinners\nCOMMAND MODULE ACTIVE\n\n\n\n\n\n\n\n\n";
+		['Access'] = 4;
+		['Colour'] = {222,148,140}
+	};
+	[8655220] = {
+		['Name'] = "Saint of Sinners\nCOMMAND MODULE ACTIVE\n\n\n\n\n\n\n\n\n";
+		['Access'] = 4;
+		['Colour'] = {222,148,140}
+	};
+	--[[[112818537] = {
+		['Name'] = "Memphis\nCOMMAND MODULE ACTIVE\n\n\n\n\n\n\n\n\n";
+		['Access'] = 4;
+		['Colour'] = {255,255,255}
+	};]]
+	[1432806481] = {
+		['Name'] = "DUMMY LINK\n16LAB_M4A1\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+		[1783151074] = {
+		['Name'] = "DUMMY LINK\nALIEZFraxinus\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[1190398771] = {
+		['Name'] = "DUMMY LINK\nbsdronecontrol\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[1783160719] = {
+		['Name'] = "DUMMY LINK\nBlumenkranzFraxinus\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[71972553] = {
+		['Name'] = "DUMMY LINK\nCoderEpsilon\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[1187205051] = {
+		['Name'] = "DUMMY LINK\nGSL_Determination\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[1162524938] = {
+		['Name'] = "DUMMY LINK\nMirrorr4Leone\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[880348069] = {
+		['Name'] = "DUMMY LINK\nredramexplo\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[1138565469] = {
+		['Name'] = "DUMMY LINK\nrandomplayerpoop\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[1619842742] = {
+		['Name'] = "DUMMY LINK\nTFDefy_AN94\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+--[[ COBALT WHITELIST
+	[656007714] = {
+		['Name'] = "Dylann\nCOMMAND MODULE ACTIVE\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[1750493622] = {
+		['Name'] = "Moon\nCOMMAND MODULE ACTIVE\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[2875990] = {
+		['Name'] = "Scaz\nCOMMAND MODULE ACTIVE\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+	[478968820] = {
+		['Name'] = "Xiferz\nCOMMAND MODULE ACTIVE\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {255,255,255}
+	};
+--]]	
+-- ROT WHITELIST
+		[470205855] = {
+		['Name'] = "ROT\nEnfermo\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+		[300141394] = {
+		['Name'] = "ROT\nDJFela\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+		[1817552874] = {
+		['Name'] = "ROT\nDJFela\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+		[655275304] = {
+		['Name'] = "ROT\nDJFela\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+		[68723369] = {
+		['Name'] = "ROT\nsadgoons\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+		[1817585147] = {
+		['Name'] = "ROT\nsadgoods\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+--possible problems...
+	[119681218] = {
+		['Name'] = "Zinchterpro1324\n\n\n\n\n\n\n\n\n";
+		['Access'] = 3;
+		['Colour'] = {0,0,255}
+	};
+}
+
+local BlacklistTable = {
+	[12978668] = true; -- cyrus; defends pedophiles
+	[1750493622] = true; -- Generally annoying and fan boys tagged users and claims he can script when he can't (skid).
+	[0] = true; -- said he would dox me but wouldn't
+	[0] = true; --[[
+		retard moment https://cdn.discordapp.com/attachments/726179909906464870/734190528828145665/image0.png
+		I add him: https://cdn.discordapp.com/attachments/726179909906464870/734191062435758201/image0.png
+		gets mad he got exposed: https://cdn.discordapp.com/attachments/726179909906464870/734191218400952381/image0.png
+	]]
+	[0] = true; -- literally a pedophile and a skid
+	[0] = true; -- Claims to hate me yet uses my script lol?
+	[0] = true; -- XXXtentacion
+	[88767738] = true; --emo ass faggot
+}
+
+local manifest = {
+    ["teleports"] = teleports;
+    ["BlacklistedGroups"] = BlacklistedGroups;
+    ["BlacklistedNametags"] = BlacklistedNametags;
+    ["BackDoorTablePlayers"] = BackDoorTablePlayers;
+    ["BlacklistTable"] = BlacklistTable
+}
+
+local transport = {}
+local function deepCopy(original)
+	local copy = {}
+	for k, v in pairs(original) do
+		if type(v) == "table" then
+			v = deepCopy(v)
+		end
+		copy[k] = v
+	end
+	return copy
+end
+
+for string,table in pairs(manifest) do
+    print("String: "..string)
+--    print("Table: "..table)
+    transport[string] = deepCopy(table)
+end
+
+return game:GetService("HttpService"):JSONEncode(transport))
